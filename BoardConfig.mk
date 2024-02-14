951382687c298b854c68623b66a8c4275a266847
dcljla <60165461+dcljla@users.noreply.github.com>
2024-02-14 10:19:28 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -87,7 +87,7 @@ TARGET_BOARD_PLATFORM_GPU := mali-t760mp8
 
 # TWRP Configuration
 TW_THEME := portrait_hdpi
-TW_EXTRA_LANGUAGES := true
+TW_EXTRA_LANGUAGES := false
 #TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
